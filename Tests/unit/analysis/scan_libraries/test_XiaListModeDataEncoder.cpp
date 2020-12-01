068d30ff130f126707198b53c234cada3ac0a240
S. V. Paulauskas <stanpaulauskas@gmail.com>
2020-12-01 17:39:44 -0500

    #86: test_XiaListModeDataEncoder works as expected

--- Tests/unit/analysis/scan_libraries/test_XiaListModeDataEncoder.cpp
+++ Tests/unit/analysis/scan_libraries/test_XiaListModeDataEncoder.cpp
@@ -5,7 +5,7 @@
 #include "XiaListModeDataEncoder.hpp"
 #include "UnitTestSampleData.hpp"
 
-#include <UnitTest++.h>
+#include "doctest.h"
 
 using namespace std;
 using namespace DataProcessing;
@@ -13,117 +13,111 @@ using namespace unittest_encoded_data;
 using namespace unittest_decoded_data;
 using namespace unittest_encoded_data::R30474_250;
 
-void SetupXiaDataClass(XiaData &data) {
-    data.Initialize();
-    data.SetEnergy(energy);
-    data.SetSlotNumber(slotId);
-    data.SetChannelNumber(channelNumber);
-    data.SetCrateNumber(crateId);
-    data.SetEventTimeLow(ts_low);
-    data.SetEventTimeHigh(ts_high);
-}
+TEST_SUITE ("Analysis/Scan Libraries/XiaListModeDataEncoder") {
+    void SetupXiaDataClass(XiaData &data) {
+        data.Initialize();
+        data.SetEnergy(energy);
+        data.SetSlotNumber(slotId);
+        data.SetChannelNumber(channelNumber);
+        data.SetCrateNumber(crateId);
+        data.SetEventTimeLow(ts_low);
+        data.SetEventTimeHigh(ts_high);
+    }
 
-void SetupEsums(XiaData &data) {
-    data.SetFilterBaseline(unittest_decoded_data::filterBaseline);
-    data.SetEnergySums(unittest_decoded_data::energy_sums);
-}
+    void SetupEsums(XiaData &data) {
+        data.SetFilterBaseline(unittest_decoded_data::filterBaseline);
+        data.SetEnergySums(unittest_decoded_data::energy_sums);
+    }
 
-void SetupExternalTimestamp(XiaData &data) {
-    data.SetExternalTimeLow(unittest_decoded_data::ex_ts_low);
-    data.SetExternalTimeHigh(unittest_decoded_data::ex_ts_high);
-}
+    void SetupExternalTimestamp(XiaData &data) {
+        data.SetExternalTimeLow(unittest_decoded_data::ex_ts_low);
+        data.SetExternalTimeHigh(unittest_decoded_data::ex_ts_high);
+    }
 
-///For now we use the same encoder for everybody since we're only testing one firmware/frequency combo.
-XiaListModeDataEncoder encoder(test_firmware, test_frequency);
+    ///For now we use the same encoder for everybody since we're only testing one firmware/frequency combo.
+    XiaListModeDataEncoder encoder(test_firmware, test_frequency);
 
-///Everybody is going to use the same XiaData class we'll just initialize it each time.
-XiaData data;
+    ///Everybody is going to use the same XiaData class we'll just initialize it each time.
+    XiaData data;
 
-TEST(TestConstructors){
-    XiaListModeDataEncoder encoder1(XiaListModeDataMask(test_firmware, test_frequency));
-    XiaListModeDataEncoder encoder2(test_firmware, test_frequency);
-}
+    TEST_CASE ("TestConstructors") {
+        XiaListModeDataEncoder encoder1(XiaListModeDataMask(test_firmware, test_frequency));
+        XiaListModeDataEncoder encoder2(test_firmware, test_frequency);
+    }
 
-TEST(TestEmptyDataThrow) {
-    CHECK_THROW(encoder.EncodeXiaData(XiaData()), invalid_argument);
-}
+    TEST_CASE ("TestEmptyDataThrow") {
+        CHECK_THROWS_AS(encoder.EncodeXiaData(XiaData()), invalid_argument);
+    }
 
-TEST(TestEncodingBasicHeader) {
-    SetupXiaDataClass(data);
-    CHECK_ARRAY_EQUAL(vector<unsigned int>(header.begin() + 2, header.end()), encoder.EncodeXiaData(data), header.size() - 2);
-}
+    TEST_CASE ("TestEncodingBasicHeader") {
+        SetupXiaDataClass(data);
+        CHECK(vector<unsigned int>(header.begin() + 2, header.end()) == encoder.EncodeXiaData(data));
+    }
 
-TEST(TestEncodingExternalTimestamps) {
-    SetupXiaDataClass(data);
-    SetupExternalTimestamp(data);
-    CHECK_ARRAY_EQUAL(vector<unsigned int>(headerWithExternalTimestamp.begin() + 2, headerWithExternalTimestamp.end()),
-                      encoder.EncodeXiaData(data), headerWithExternalTimestamp.size() - 2);
-}
+    TEST_CASE ("TestEncodingExternalTimestamps") {
+        SetupXiaDataClass(data);
+        SetupExternalTimestamp(data);
+        CHECK(vector<unsigned int>(headerWithExternalTimestamp.begin() + 2, headerWithExternalTimestamp.end()) ==
+              encoder.EncodeXiaData(data));
+    }
 
-TEST(TestEncodingEnergySums) {
-    SetupXiaDataClass(data);
-    SetupEsums(data);
-    CHECK_ARRAY_EQUAL(vector<unsigned int>(headerWithEnergySums.begin() + 2, headerWithEnergySums.end()),
-                              encoder.EncodeXiaData(data), headerWithEnergySums.size() - 2);
-}
+    TEST_CASE ("TestEncodingEnergySums") {
+        SetupXiaDataClass(data);
+        SetupEsums(data);
+        CHECK(vector<unsigned int>(headerWithEnergySums.begin() + 2, headerWithEnergySums.end()) ==
+              encoder.EncodeXiaData(data));
+    }
 
-TEST(TestEncodingEnergySumsAndExternalTimeStamps) {
-    SetupXiaDataClass(data);
-    SetupEsums(data);
-    SetupExternalTimestamp(data);
-    CHECK_ARRAY_EQUAL(vector<unsigned int>(headerWithEnergySumsExternalTimestamp.begin() + 2,
-                                           headerWithEnergySumsExternalTimestamp.end()),
-                      encoder.EncodeXiaData(data), headerWithEnergySumsExternalTimestamp.size() - 2);
-}
+    TEST_CASE ("TestEncodingEnergySumsAndExternalTimeStamps") {
+        SetupXiaDataClass(data);
+        SetupEsums(data);
+        SetupExternalTimestamp(data);
+        CHECK(vector<unsigned int>(headerWithEnergySumsExternalTimestamp.begin() + 2,
+                                   headerWithEnergySumsExternalTimestamp.end()) == encoder.EncodeXiaData(data));
+    }
 
-TEST(TestEncodingQdc) {
-    SetupXiaDataClass(data);
-    data.SetQdc(qdc);
-    CHECK_ARRAY_EQUAL(vector<unsigned int>(headerWithQdc.begin() + 2, headerWithQdc.end()),
-                      encoder.EncodeXiaData(data), headerWithQdc.size() - 2);
-}
+    TEST_CASE ("TestEncodingQdc") {
+        SetupXiaDataClass(data);
+        data.SetQdc(qdc);
+        CHECK(vector<unsigned int>(headerWithQdc.begin() + 2, headerWithQdc.end()) == encoder.EncodeXiaData(data));
+    }
 
-TEST(TestEncodingQdcAndExternalTimestamp) {
-    SetupXiaDataClass(data);
-    data.SetQdc(qdc);
-    SetupExternalTimestamp(data);
-    CHECK_ARRAY_EQUAL(vector<unsigned int>(headerWithQdcExternalTimestamp.begin() + 2, headerWithQdcExternalTimestamp.end()),
-                      encoder.EncodeXiaData(data), headerWithQdcExternalTimestamp.size() - 2);
-}
+    TEST_CASE ("TestEncodingQdcAndExternalTimestamp") {
+        SetupXiaDataClass(data);
+        data.SetQdc(qdc);
+        SetupExternalTimestamp(data);
+        CHECK(vector<unsigned int>(headerWithQdcExternalTimestamp.begin() + 2, headerWithQdcExternalTimestamp.end()) ==
+              encoder.EncodeXiaData(data));
+    }
 
-TEST(TestEncodingEnergySumsAndQdc) {
-    SetupXiaDataClass(data);
-    SetupEsums(data);
-    data.SetQdc(qdc);
-    CHECK_ARRAY_EQUAL(vector<unsigned int>(headerWithEnergySumsQdc.begin() + 2, headerWithEnergySumsQdc.end()),
-                      encoder.EncodeXiaData(data), headerWithEnergySumsQdc.size());
-}
+    TEST_CASE ("TestEncodingEnergySumsAndQdc") {
+        SetupXiaDataClass(data);
+        SetupEsums(data);
+        data.SetQdc(qdc);
+        CHECK(vector<unsigned int>(headerWithEnergySumsQdc.begin() + 2, headerWithEnergySumsQdc.end()) ==
+              encoder.EncodeXiaData(data));
+    }
 
-TEST(TestEncodingEnergySumsAndQdcAndTimestamp) {
-    SetupXiaDataClass(data);
-    SetupEsums(data);
-    data.SetQdc(qdc);
-    SetupExternalTimestamp(data);
-    CHECK_ARRAY_EQUAL(vector<unsigned int>(headerWithEnergySumsQdcExternalTimestamp.begin() + 2,
-                                           headerWithEnergySumsQdcExternalTimestamp.end()),
-                      encoder.EncodeXiaData(data), headerWithEnergySumsQdcExternalTimestamp.size() - 2);
-}
+    TEST_CASE ("TestEncodingEnergySumsAndQdcAndTimestamp") {
+        SetupXiaDataClass(data);
+        SetupEsums(data);
+        data.SetQdc(qdc);
+        SetupExternalTimestamp(data);
+        CHECK(vector<unsigned int>(headerWithEnergySumsQdcExternalTimestamp.begin() + 2,
+                                   headerWithEnergySumsQdcExternalTimestamp.end()) == encoder.EncodeXiaData(data));
+    }
 
-TEST(TestEncodingTrace) {
-    SetupXiaDataClass(data);
-    data.SetTrace(unittest_trace_variables::trace);
-    CHECK_ARRAY_EQUAL(vector<unsigned int>(headerWithTrace.begin() + 2, headerWithTrace.end()),
-                      encoder.EncodeXiaData(data), headerWithTrace.size() - 2);
-}
+    TEST_CASE ("TestEncodingTrace") {
+        SetupXiaDataClass(data);
+        data.SetTrace(unittest_trace_variables::trace);
+        CHECK(vector<unsigned int>(headerWithTrace.begin() + 2, headerWithTrace.end()) == encoder.EncodeXiaData(data));
+    }
 
-TEST(TestEncodingTraceAndQdc) {
-    SetupXiaDataClass(data);
-    data.SetQdc(qdc);
-    data.SetTrace(unittest_trace_variables::trace);
-    CHECK_ARRAY_EQUAL(vector<unsigned int>(headerWithQdcTrace.begin() + 2, headerWithQdcTrace.end()),
-                      encoder.EncodeXiaData(data), headerWithQdcTrace.size() - 2);
-}
-
-int main(int argv, char *argc[]) {
-    return (UnitTest::RunAllTests());
+    TEST_CASE ("TestEncodingTraceAndQdc") {
+        SetupXiaDataClass(data);
+        data.SetQdc(qdc);
+        data.SetTrace(unittest_trace_variables::trace);
+        CHECK(
+        vector<unsigned int>(headerWithQdcTrace.begin() + 2, headerWithQdcTrace.end()) == encoder.EncodeXiaData(data));
+    }
 }
