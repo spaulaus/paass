7df035a3bb912f0c81e9e3614da53edda010b99f
S. V. Paulauskas <stanpaulauskas@gmail.com>
2020-12-01 18:22:18 -0500

    #86: test_RootHandler works as expected

--- Tests/unit/analysis/utkscan/core/test_RootHandler.cpp
+++ Tests/unit/analysis/utkscan/core/test_RootHandler.cpp
@@ -2,35 +2,28 @@
 /// @brief Unittests for the RootHandler class.
 /// @author S. V. Paulauskas
 /// @date February 24, 2018
-/// @copyright Copyright (c) 2018 S. V. Paulauskas. 
-/// @copyright All rights reserved. Released under the Creative Commons Attribution-ShareAlike 4.0 International License
 #include "RootHandler.hpp"
 
-#include <UnitTest++.h>
+#include "doctest.h"
 
-#include <chrono>
-#include <random>
+TEST_SUITE ("Analysis/Utkscan/Core/Root Handler") {
+    TEST_CASE ("RootHandler") {
+        RootHandler *handler = RootHandler::get("/tmp/test-RootHandler");
+        CHECK(handler);
 
-TEST(TestRootHandler) {
-    RootHandler *handler = RootHandler::get("/tmp/unittest-RootHandler");
-    CHECK(handler);
+        CHECK("TH1D" == std::string(handler->RegisterHistogram(0, "test1d", 10)->ClassName()));
+        CHECK("TH2D" == std::string(handler->RegisterHistogram(1, "test2d-xy", 10, 10)->ClassName()));
+        CHECK("TH2D" == std::string(handler->RegisterHistogram(2, "test2d-xz", 10, 0, 10)->ClassName()));
+        CHECK("TH3D" == std::string(handler->RegisterHistogram(3, "test3d", 10, 10, 10)->ClassName()));
 
-    CHECK_EQUAL("TH1D", handler->RegisterHistogram(0, "test1d", 10)->ClassName());
-    CHECK_EQUAL("TH2D", handler->RegisterHistogram(1, "test2d-xy", 10, 10)->ClassName());
-    CHECK_EQUAL("TH2D", handler->RegisterHistogram(2, "test2d-xz", 10, 0, 10)->ClassName());
-    CHECK_EQUAL("TH3D", handler->RegisterHistogram(3, "test3d", 10, 10, 10)->ClassName());
+        CHECK(!handler->Plot(123, 123));
+        CHECK_THROWS_AS(handler->Get1DHistogram(123), std::invalid_argument);
+        CHECK_THROWS_AS(handler->Get2DHistogram(123), std::invalid_argument);
+        CHECK_THROWS_AS(handler->Get3DHistogram(123), std::invalid_argument);
 
-    CHECK(!handler->Plot(123,123));
-    CHECK_THROW(handler->Get1DHistogram(123), std::invalid_argument);
-    CHECK_THROW(handler->Get2DHistogram(123), std::invalid_argument);
-    CHECK_THROW(handler->Get3DHistogram(123), std::invalid_argument);
+        unsigned int dummy = 0;
+        CHECK_THROWS_AS(handler->RegisterBranch("notatree", "branchname", &dummy, "leaf list"), std::invalid_argument);
 
-    unsigned int dummy = 0;
-    CHECK_THROW(handler->RegisterBranch("notatree", "branchname", &dummy, "leaf list"), std::invalid_argument);
-
-    delete RootHandler::get();
-}
-
-int main(int argv, char *argc[]) {
-    return (UnitTest::RunAllTests());
+        delete RootHandler::get();
+    }
 }
